076640569314d833e9d6a3a2570c664f46b84777
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-11 00:00:59 +0100

    0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """check if all boxes can be opened"""
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key >= 0 and key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
