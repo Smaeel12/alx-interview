485982b698c02c8a6670776d1075c4f74f5da760
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-12 21:37:42 +0100

    add files

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-# Adjacency matrices
+""" Module contain canUnlockAll method
+"""
 from collections import deque
 
+
 def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened
+    """
     opened = [0]
     keys = boxes[0]
 
@@ -10,8 +14,9 @@ def canUnlockAll(boxes):
         if k not in opened:
             opened.append(k)
             try:
-                keys.extend([newkey for newkey in boxes[k] if newkey not in opened])
-            except:
+                keys.extend([newkey for newkey in boxes[k]
+                             if newkey not in opened])
+            except Exception:
                 pass
     if (len(opened) == len(boxes)):
         return True
