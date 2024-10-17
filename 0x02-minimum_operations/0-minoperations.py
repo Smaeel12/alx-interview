f804f2da02261b2aeb9259641029b4174dfbb191
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-17 10:07:56 +0100

    V1

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Given a number n,  minOperations method calculates the fewest number of
+ operations needed to result in exactly n H characters in the file.
+"""
+
+
+def is_prime(num: int) -> bool:
+    """Check prime numbers"""
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def minOperations(n: int) -> int:
+    """Calculate the min ops"""
+    memo = []
+    prime_factors = []
+    i = 2
+
+    while True:
+        if n == 0 or n < i:
+            sum = 0
+            for i in prime_factors:
+                sum += i
+            return sum
+        if i in memo or is_prime(i):
+            print(memo, i)
+            if i not in memo:
+                memo.append(i)
+            if n % i == 0:
+                prime_factors.append(i)
+                n = n / i
+                i = 1
+        i += 1
